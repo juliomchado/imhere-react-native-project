286a8b74897dac58f4f0aa15a90c1210432115f2
Julio Machado <juliocarlos00@hotmail.com>
2024-09-11 02:22:42 +0100

    Initial Project

---
+++ App.tsx
@@ -0,0 +1,31 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: "#131016",
+      }}
+    >
+      <Text
+        style={{
+          color: "#FDFCFE",
+          fontSize: 24,
+          fontWeight: "bold",
+          marginTop: 48,
+        }}
+      >
+        React
+      </Text>
+      <Text
+        style={{
+          color: "#6B6B6B",
+          fontSize: 16,
+        }}
+      >
+        Quarta feira, 11/09/2024 02:16
+      </Text>
+    </View>
+  );
+}
